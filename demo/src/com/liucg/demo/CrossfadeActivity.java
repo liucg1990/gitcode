a719d2a81a95f4299c55db4b2caca9efd8a287a8
liuchuanguang <liuchuanguang@kugou.net>
2014-08-22 10:47:21 +0800

    modify CrossfadeActivity

--- demo/src/com/liucg/demo/CrossfadeActivity.java
+++ demo/src/com/liucg/demo/CrossfadeActivity.java
@@ -1,5 +1,7 @@
 package com.liucg.demo;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,6 +26,30 @@ public class CrossfadeActivity extends Activity {
 		// Retrieve and cache the system's default "short" animation time.
 		mShortAnimationDuration = getResources().getInteger(
 				android.R.integer.config_shortAnimTime);
+	}
+
+	private void crossfade() {
+
+		// Set the content view to 0% opacity but visible, so that it is visible
+		// (but fully transparent) during the animation.
+		mContentView.setAlpha(0f);
+		mContentView.setVisibility(View.VISIBLE);
+
+		// Animate the content view to 100% opacity, and clear any animation
+		// listener set on the view.
+		mContentView.animate().alpha(1f).setDuration(mShortAnimationDuration)
+				.setListener(null);
+
+		// Animate the loading view to 0% opacity. After the animation ends,
+		// set its visibility to GONE as an optimization step (it won't
+		// participate in layout passes, etc.)
+		mLoadingView.animate().alpha(0f).setDuration(mShortAnimationDuration)
+				.setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						mLoadingView.setVisibility(View.GONE);
+					}
+				});
 	}
 
 }
