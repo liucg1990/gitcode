ca10506808ab3aebcd8cad73f604b09241169545
liuchuanguang <liuchuanguang@kugou.net>
2014-08-22 11:12:20 +0800

    add ScreenSlidePageFragment

--- demo/gen/com/liucg/demo/R.java
+++ demo/gen/com/liucg/demo/R.java
@@ -31,6 +31,8 @@ public final class R {
     public static final class layout {
         public static final int activity_crossfade=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_screen_slide=0x7f030002;
+        public static final int fragment_screen_slide_page=0x7f030003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
