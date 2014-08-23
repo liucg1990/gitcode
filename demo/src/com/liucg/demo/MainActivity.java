667fbc9c6f47592a6a3f666ce7cde1718b84ccb8
liucg1990 <liucg1990@gmail.com>
2014-08-23 19:17:56 +0800

    add palyerActivity

--- demo/src/com/liucg/demo/MainActivity.java
+++ demo/src/com/liucg/demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.liucg.demo;
 
+import player.PlayerActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Intent intent =new Intent(this, PlayerActivity.class);
+		startActivity(intent);
 	}
 
 }
---
+++ demo/src/player/PlayerActivity.java
@@ -0,0 +1,15 @@
+package player;
+
+import com.liucg.demo.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_player);
+	}
+
+}
